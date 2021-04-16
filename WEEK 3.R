3583cbc404099d8b2e945daa8b0d1d40a53b9767
Jager1001 <81004709+Jager1001@users.noreply.github.com>
2021-04-16 10:40:12 -0600

    Add files via upload

---
+++ WEEK 3.R
@@ -0,0 +1,31 @@
+## I simply set the input x as a matrix
+## and then set the solved value "j" as a null
+## then I changed every reference to "mean" to "Inverse"
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+## Same here, changed "mean" to "solve" and "m" to "j"
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
