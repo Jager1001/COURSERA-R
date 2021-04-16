638d827dd265bf3d4d45553dffc93f6c4158f43a
Jager1001 <81004709+Jager1001@users.noreply.github.com>
2021-04-15 18:42:34 -0600

    Add files via upload

---
+++ Coursera.R
@@ -0,0 +1,25 @@
+makecachematrix <- function(x = numeric()) {
+  inversa<- NULL
+  set <- function(y) {
+    x <<- y
+    inversa <<- NULL
+  }
+  get <- function() x
+  setinversa <- function(inversacalculada) inversa<<- inversacalculada
+  getinversa <- function() inversa
+  list(set = set, get = get,
+       setinversa = setinversa,
+       getinversa = getinversa)
+}
+
+cachesolve <- function(x, ...) {
+  inversa<- x$getinversa()
+  if(!is.null(inversa)) {
+    message("getting cached data")
+    return(inversa)
+  }
+  data <- x$get()
+  inversa <- solve(data, ...)
+  x$setinversa(inversa)
+  inversa
+}
